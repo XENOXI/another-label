b012ff44ce1f869d13745245aa2d298dbfc2ed5e
AZAZZZEL <shifer.k0d@gmail.com>
2024-03-01 08:57:12 +0700

    timeline #2

--- persontimeline.py
+++ persontimeline.py
@@ -16,7 +16,8 @@ class PersonTimeline(QWidget):
         self.labels = None
         self.frame_cnt = 0
         self.bbox_cnt = 10
-        
+        self.unique_bbox = 0
+        self.sequences = []
 
 
     def paintEvent(self, a0: QPaintEvent | None) -> None:
@@ -26,18 +27,22 @@ class PersonTimeline(QWidget):
         for i in range(0,self.frame_cnt):
             painter.drawRect((i-self.frame)*self.frame_box_size, 10, self.frame_box_size, 100)
 
+        if self.labels is None:
+            return
+
         brush = QBrush(Qt.GlobalColor.magenta,Qt.BrushStyle.SolidPattern)
         painter.setBrush(brush)
         painter.translate(self.frame_box_size/2-self.frame*self.frame_box_size,5+self.frame_box_size//2)
 
-        
-        for i in range(0,100):
-            painter.fillRect(0, (self.frame_rhomb_size)//2, self.frame_rhomb_size*2,self.connection_heigth,QBrush(QColor(200,0,200)))
-            painter.rotate(45)
-            painter.drawRect(0, 0, self.frame_rhomb_size, self.frame_rhomb_size)
-            painter.rotate(-45)
-            painter.translate(self.frame_box_size,0)
-
+        for seq in self.sequences: 
+            size = seq.shape[0]     
+            for i in range(0,size):
+                painter.fillRect(0, (self.frame_rhomb_size)//2, self.frame_rhomb_size*2,self.connection_heigth,QBrush(QColor(200,0,200)))
+                painter.rotate(45)
+                painter.drawRect(0, 0, self.frame_rhomb_size, self.frame_rhomb_size)
+                painter.rotate(-45)
+                painter.translate(self.frame_box_size,0)
+            painter.translate(self.frame_box_size*size,self.frame_box_size)
 
 
     def set_frame(self,frame):
@@ -49,8 +54,11 @@ class PersonTimeline(QWidget):
         self.repaint()
     
     def set_labels(self, labels:pd.DataFrame):
-        self.labels = labels    
+        self.labels = labels  
         self.unique_bbox = pd.unique(labels["track_id"])
+        self.sequences = []
+        for i in range(self.unique_bbox):
+            self.sequences.append(labels[labels["track_id"]==i].copy().sort_index("frame",ascending=True))
 
     def sizeHint(self) -> QSize:
         return QSize(1000, self.bbox_cnt*self.frame_box_size+20)
